bc83a99808f123f624f931c3167ec4c689921a96
Alex Panov <alex@insighty.studio>
2019-09-11 18:13:34 -0400

    Move hidden files

---
+++ App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './App';
+
+import renderer from 'react-test-renderer';
+
+it.skip('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
